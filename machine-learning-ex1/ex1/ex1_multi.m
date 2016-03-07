cffb86e9e4c2dcc1f26ebfca5fdfcd3a8eaa4ca0
brudder003 <rudder.brett@gmail.com>
2016-03-06 21:42:51 -0600

    normal eq, completed

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -83,8 +83,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 1500;
+alpha = 0.1;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -156,7 +156,9 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+house_n = [1 1650 3];
+
+price = house_n*theta; % You should change this
 
 
 % ============================================================
