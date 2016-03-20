f072ba96546b0a6ec60a751d62589b99285097c3
brudder003 <rudder.brett@gmail.com>
2016-03-20 17:48:00 -0500

    nn cost and grad done

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,12 +36,23 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+theta_s = theta(2:size(theta));
+theta_reg = [0;theta_s];
 
+% cost and grandient just like before
+cost = -1/m*(log((sigmoid(X*theta))')*y+log((1-sigmoid(X*theta))')*(1-y));
+grad = (1/m).*X'*(sigmoid(X*theta)-y);
 
+% compute the regularized cost
+J = cost + (lambda / (2*m)) * sum(theta_reg .^ 2);
 
+% compute regularized grad
+reg_vector = (lambda / m) * theta;
+% set first row to zero and add this to grad
+reg_vector(1) = 0;
 
-
-
+%add the gradient and the regularization vector
+grad = grad + reg_vector;
 
 
 
