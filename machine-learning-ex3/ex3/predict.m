92d0b1867c501e496b1dfcc0967b6e1655e8cdda
brudder003 <rudder.brett@gmail.com>
2016-03-20 22:56:41 -0500

    ex3 complete

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,6 +21,29 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% X is 5000 x 400 right now, need to add ones to the beginning
+% for the bias node
+a1 = [ones(m,1) X];
+
+% just following the diagram in the homework, calculate z for the 
+% hidden layer a1 is 5000x401 theta1 is 25x401 need to transpose
+% to multiply a1*theta'
+z2 = a1*Theta1';
+
+% a2 is sigmoid of z2 and add the ones for the next layer
+a2 = [ones(size(z2),1) sigmoid(z2)];
+
+% calc z3 for the output layer
+z3 = a2*Theta2';
+
+% a3 (output) is sigmoid z3
+a3 = sigmoid(z3);
+
+%obtain max for each row like the one v all method
+[predict_max, index_max] = max(a3, [], 2);
+
+%return p
+p = index_max;
 
 
 
