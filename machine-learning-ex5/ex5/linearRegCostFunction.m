bc0b55bde6626e79df29f50a235dd2e1950471b7
brudder003 <rudder.brett@gmail.com>
2016-07-05 22:03:34 -0500

    ex 5

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -10,6 +10,8 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
+cost=0;
+reg=0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
@@ -19,14 +21,28 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+%make theata zero equal to zero so it doesnt contriubute to the sum;
+theta_s = theta(2:size(theta));
+theta_reg = [0;theta_s];
 
 
+%vectorized implempentation of linear regression cost function
+cost = 1/(2*m)*((X*theta - y)'*(X*theta - y));
 
+reg = (lambda / (2*m)) * sum(theta_reg .^ 2);
 
+J=cost + reg;
 
+%vectorized implementation of the gradient
+grad = (1/m).*X'*((X*theta)-y);
 
+% compute regularized grad
+reg_vector = (lambda / m) * theta;
+% set first row to zero and add this to grad
+reg_vector(1) = 0;
 
-
+%add the gradient and the regularization vector
+grad = grad + reg_vector;
 
 
 
