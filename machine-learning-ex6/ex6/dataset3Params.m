e4dbc6831f4c600aecf33fe7ab652a139d44b8d7
brudder003 <rudder.brett@gmail.com>
2016-07-12 19:55:31 -0500

    ex6 complete

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,40 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%as suggested in the pdf for the assignment try these
+params = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+%from the note above, using this to compute the prediction errror
+error_min=inf;
+error=inf;
+C_final = 0;
+sigma_final = 0;
+
+%double loop over C and sigmaaaaa
+for C = params
+  for sigma = params
+  
+  %train model with this particular sigma and C
+   model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+   
+   %compute prediction error
+   predictions = svmPredict(model, Xval);
+   error = mean(double(predictions ~= yval));
+   
+   %if statement to organize error
+   if (error<=error_min)
+    C_final = C;
+    sigma_final = sigma;
+    error_min=error;
+   end
+  end
+ end
+ 
+ C=C_final;
+ sigma=sigma_final;
 
 
-
-
+fprintf('Best value C, sigma = [%f %f] with prediction error = %f\n', C, sigma, error_min);
 
 
 % =========================================================================
