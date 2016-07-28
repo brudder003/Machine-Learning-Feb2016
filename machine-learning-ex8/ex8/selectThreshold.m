09b8af5c845e37d06f9d42420db47eebb05b424e
brudder003 <rudder.brett@gmail.com>
2016-07-28 17:41:29 -0500

    almost through second part

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -1,4 +1,4 @@
-function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+function [bestEpsilon bestF1, bpred] = selectThreshold(yval, pval)
 %SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
 %outliers
 %   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
@@ -22,24 +22,40 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
+    
+    %f1 score = 2*precision*recall/(precision + recall)
+    %where
+    %precision = true positive / (true positive + false neg)
+    %recall = true positive / (true pos + false pos)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    %predictions like suggested above
+    predictions = zeros(size(pval,1),1);
+    predictions = (pval < epsilon);
+    
+    %true positives = y says 1 and predictions says 1
+    tp = sum((predictions==1) & (yval==1));
+    
+    %false pos = predictions = 1 and yval = 0
+    fp = sum((predictions==1) & (yval==0));
+    
+    %false neg = predictions = 0 and yval = 1
+    fn = sum((predictions==0) & (yval==0));
+    
+    %precision 
+    prec = tp/(tp+fp);
+    
+    %recall
+    recall = tp/(tp+fn);
+    
+    %score it!
+    F1 = (2*prec*recall)/(prec + recall);
+   
     % =============================================================
 
     if F1 > bestF1
        bestF1 = F1;
        bestEpsilon = epsilon;
+       bpred = predictions;
     end
 end
 
